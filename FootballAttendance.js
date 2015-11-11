a4e03683bf98278bc9ed6f30beaff7849172a0ab
Adam Humphrey <adamthumph@gmail.com>
2015-11-11 16:47:16 -0600

    Uploading all of the files

---
+++ FootballAttendance.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+	var image = document.getElementById('myImage');
+	var buttons = document.getElementsByClassName('myButton');
+	
+	//Buttons to change images functios
+	buttons[0].onclick = function changeImagetoRice2011() { image.src="GameImages/2011/Rice2011.jpg"; };
+	buttons[1].onclick = function changeImagetoBYU2011() { image.src="GameImages/2011/BYU2011.jpg"; };
+	buttons[2].onclick = function changeImagetoOkSt2011() { image.src="GameImages/2011/OkSt2011.jpg"; };
+	buttons[3].onclick = function changeImagetoKansas2011() { image.src="GameImages/2011/Kansas2011.jpg"; };
+	buttons[4].onclick = function changeImagetoTech2011() { image.src="GameImages/2011/Tech2011.jpg"; };
+	buttons[5].onclick = function changeImagetoKSt2011() { image.src="GameImages/2011/KState2011.jpg"; };
+	buttons[6].onclick = function changeImagetoWyoming2012() { image.src="GameImages/2012/Wyoming2012.jpg"; };
+	buttons[7].onclick = function changeImagetoNewMexico2012() { image.src="GameImages/2012/NewMexico2012.jpg"; };
+	buttons[8].onclick = function changeImagetoWVU2012() { image.src="GameImages/2012/WVU2012.jpg"; };
+	buttons[9].onclick = function changeImagetoBaylor2012() { image.src="GameImages/2012/Baylor2012.jpg"; };
+	buttons[10].onclick = function changeImagetoIowaState2012() { image.src="GameImages/2012/IowaState2012.jpg"; };
+	buttons[11].onclick = function changeImagetoTCU2012() { image.src="GameImages/2012/TCU2012.jpg"; };
+	buttons[12].onclick = function changeImagetoNMSt2013() { image.src="GameImages/2013/NMSt2013.jpg"; };
+	buttons[13].onclick = function changeImagetoOleMiss2013() { image.src="GameImages/2013/OleMiss2013.jpg"; };
+	buttons[14].onclick = function changeImagetoKSt2013() { image.src="GameImages/2013/KSt2013.jpg"; };
+	buttons[15].onclick = function changeImagetoKansas2013() { image.src="GameImages/2013/Kansas2013.jpg"; };
+	buttons[16].onclick = function changeImagetoOkSt2013() { image.src="GameImages/2013/OkSt2013.jpg"; };
+	buttons[17].onclick = function changeImagetoTech2013() { image.src="GameImages/2013/Tech2013.jpg"; };
+	buttons[18].onclick = function changeImagetoNorthTexas2014() { image.src="GameImages/2014/NorthTexas2014.jpg"; };
+	buttons[19].onclick = function changeImagetoBYU2014() { image.src="GameImages/2014/BYU2014.jpg"; };
+	buttons[20].onclick = function changeImagetoBaylor2014() { image.src="GameImages/2014/Baylor2014.jpg"; };
+	buttons[21].onclick = function changeImagetoIowaState2014() { image.src="GameImages/2014/IowaSt2014.jpg"; };
+	buttons[22].onclick = function changeImagetoWVU2014() { image.src="GameImages/2014/WVU2014.jpg"; };
+	buttons[23].onclick = function changeImagetoTCU2014() { image.src="GameImages/2014/TCU2014.jpg"; };
+	buttons[24].onclick = function changeImagetoRice2015() { image.src="GameImages/2015/Rice2015.jpg"; };
+	buttons[25].onclick = function changeImagetoCal2015() { image.src="GameImages/2015/Cal2015.jpg"; };
+	buttons[26].onclick = function changeImagetoOkSt2015() { image.src="GameImages/2015/OkSt2015.jpg"; };
+	buttons[27].onclick = function changeImagetoKSt2015() { image.src="GameImages/2015/KSt2015.jpg"; };
+	buttons[28].onclick = function changeImagetoKansas2015() { image.src="GameImages/2015/Kansas2015.jpg"; };
+
+};
